fa26716b5e350c19048f1e06d4893c0469f5817c
Anirudh Gokamalla <113572398+anirudhviswanath2001@users.noreply.github.com>
2024-09-07 14:52:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,12 @@ import base64
 
 poppler_path = '/usr/bin/'
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 if api_key:
     genai.configure(api_key=api_key)
 else:
-    raise ValueError("GOOGLE_API_KEY is not set. Please configure your environment.")
+    st.error("GOOGLE_API_KEY is not set. Please configure the API key in the environment variables.")
+    st.stop
 
 
 def get_docx(text):
